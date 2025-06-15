917a2a5f04ab79ce27982dc18d33085d573ce6f6
Christine-Muchiri <christinemuchiri749@gmail.com>
2025-06-15 21:55:45 +0300

    pydantic schema

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
-from typing import Optional
+from typing import Optional, List
+from enum import IntEnum
+from pydantic import BaseModel, Field
 from fastapi import FastAPI
 
 app = FastAPI()
+
+class Priority(IntEnum):
+    LOW = 3
+    MEDIUM = 2
+    HIGH = 1
+    
+class TodoBase(BaseModel):
+    todo_name: str = Field(..., min_length=3, max_length=512, description='Name of the todo')
+    todo_description: str = Field(..., description='Description of todo')
+    priority: Priority = Field(default=Priority.LOW, description='Priority of the todo')
+    
+class TodoCreate(TodoBase):
+    pass
+
+class Todo(TodoBase):
+    pass
 
 all_todos= [
     {'todo_id':1, 'todo_name': 'sports', 'todo_description': 'go to gym'},
@@ -29,5 +47,30 @@ def get_todos(first_n: Optional[int]= None):
     else:
         return all_todos
 
-
-@api.post('/todos')
+@app.post('/todos')
+def create_todo(todo: dict):
+    new_todo_id = max(todo['todo_id'] for todo in all_todos) + 1
+    
+    new_todo = {
+        'todo_id': new_todo_id,
+        'todo_name': todo['todo_name'],
+        'todo_description': todo['todo_description']
+    }
+    all_todos.append(new_todo)
+    
+    return new_todo
+@app.put('/todos/{todo_id}')
+def update_todo(todo_id: int, updated_todo: dict):
+    for todo in all_todos:
+        if todo['todo_id'] == todo_id:
+            todo['todo_name'] = update_todo['todo_name'] # type: ignore
+            todo['todo_description'] = update_todo['todo_description'] # type: ignore
+            return todo
+    return "Error, not found"
+        
+@app.delete('/todos/{todo_id}')
+def delete_todo(todo_id: int):
+    if todo['todo_id'] == todo_id: # type: ignore
+        deleted_todo = all_todos.pop(index) # type: ignore
+        return deleted_todo
+    return "Error, not found"
